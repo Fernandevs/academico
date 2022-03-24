4733cbe8719b92325c30b694cb0bbdd0f60afae0
Fernandevs <ortizfernando05@gmail.com>
2022-03-24 15:34:10 -0600

    Módulo de registro de usuarios terminado

--- oe/views.py
+++ oe/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def create_user(request):
+    if request.method == 'POST':
+        try:
+            username = request.POST['email']
+
+            if request.POST['password'] == request.POST['repeated-password']:
+
+                password = request.POST['password']
+
+                user = User.objects.create_user(username, username, password)
+                user.save()
+
+            return render(request, "success.html")
+
+        except Exception as e:
+            print(e)
+
+    return render(request, 'create.html')
